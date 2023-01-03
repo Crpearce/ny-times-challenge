1c1072eb84fd1d46960e804c68a916253aad1f0a
Colby Pearce <crpearce892@gmail.com>
2023-01-03 14:08:02 -0700

    add articles container, begin working out article component

---
+++ src/components/article/article.component.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./article.styles.css";
+
+const Article = ({ title, image, date, byline }) => {
+  const mm = `${date[5]}${date[6]}`;
+  const dd = `${date[8]}${date[9]}`;
+  const yy = date.slice(0, 4);
+
+  return (
+    <Link to={`/${date}`}>
+      <div className="article-card">
+        <div className="wrapper">
+          <img className="card-image" src={image} alt={title} />
+        </div>
+        <h3>{title}</h3>
+        <h4>{byline}</h4>
+        <p className="card-date">{`${mm}/${dd}/${yy}`}</p>
+      </div>
+    </Link>
+  );
+};
+
+export default Article;
