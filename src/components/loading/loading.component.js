92c0993dd29fc2f1b8a2a4653ab1855ecacd5961
Colby Pearce <crpearce892@gmail.com>
2023-01-05 11:00:26 -0700

    loading message

---
+++ src/components/loading/loading.component.js
@@ -0,0 +1,12 @@
+
+import "./loading.styles.css";
+
+const Loading = () => {
+  return (
+    <div className='loading-container'>
+      <p className='loading'>Loading articles</p>
+    </div>
+  )
+}
+
+export default Loading;
