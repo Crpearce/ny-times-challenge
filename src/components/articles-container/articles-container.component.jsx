30425fa7e632701ccb26a2bc1073c0f004b2806d
Colby Pearce <crpearce892@gmail.com>
2023-01-06 08:47:40 -0700

    add image to assets for stories with missing multimedia

--- src/components/articles-container/articles-container.component.jsx
+++ src/components/articles-container/articles-container.component.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Article from "../article/article.component";
 import "./articles-container.styles.css";
+import noImage from "../../assets/placeholder.png"
 
 const ArticlesContainer = ({ articles }) => {
   const displayCards = articles.map((article) => {
     return (
       <Article
-        image={!article.multimedia ? 'article image does not exist' : article.multimedia[0].url}
+        image={!article.multimedia ? noImage : article.multimedia[0].url}
         title={article.title}
         byline={article.byline}
         date={article.published_date}
